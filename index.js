801309969c97fa296300da44dc570e77836808c2
Metranohm <andrewwink@gmail.com>
2023-03-29 22:26:21 -0500

    create show page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+uuid();
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,22 +14,22 @@ const comments = [
   {
     username: 'Todd',
     comment: 'lol that is so funny!',
-    id: 1
+    id: uuid()
   },
   {
     username: 'Skyler',
     comment: 'I like to go birdwatching with my dog',
-    id: 2
+    id: uuid()
   },
   {
     username: 'Sk8erBoi',
     comment: 'Plz delete your account, Todd',
-    id: 3
+    id: uuid()
   },
   {
     username: 'onlysayswoof',
     comment: 'woof woof woof',
-    id: 4
+    id: uuid()
   }
 ];
 
@@ -41,13 +43,13 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
   const { username, comment } = req.body;
-  comments.push({ username, comment });
+  comments.push({ username, comment, id: uuid() });
   res.redirect('/comments');
 });
 
 app.get('/comments/:id', (req, res) => {
   const { id } = req.params;
-  const comment = comments.find(c => c.id === parseInt(id));
+  const comment = comments.find(c => c.id === id);
   res.render('comments/show', { comment });
 })
 
